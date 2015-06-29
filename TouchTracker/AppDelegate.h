92fcb996659e59b3881df1343c541206506627b4
rebornwhu <rebornwhu@gmail.com>
2015-06-28 18:04:30 -0700

    First commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Xiao Lu on 6/28/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by Xiao Lu on 6/28/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
