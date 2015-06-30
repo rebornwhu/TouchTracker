12cc255a8500b71cfc790fc1591064ee21263228
rebornwhu <rebornwhu@gmail.com>
2015-06-29 23:33:16 -0700

    Implement drawing lines

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -7,15 +7,92 @@
 //
 
 #import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDrawView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+    
+    if (self) {
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    
+    return self;
 }
-*/
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine) {
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *t = [touches anyObject];
+    CGPoint location = [t locationInView:self];
+    
+    self.currentLine = [[BNRLine alloc] init];
+    self.currentLine.begin = location;
+    self.currentLine.end = location;
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *t = [touches anyObject];
+    CGPoint location = [t locationInView:self];
+    
+    self.currentLine.end = location;
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.finishedLines addObject:self.currentLine];
+    
+    self.currentLine = nil;
+    
+    [self setNeedsDisplay];
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
